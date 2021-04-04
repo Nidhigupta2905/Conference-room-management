f1e71f769ee0ea1c9dd0a0373e81ce8f030c00dd
sanat-biswas <39663269+sanat-biswas@users.noreply.github.com>
2021-04-04 23:12:12 +0530

    admin theme fixing

--- app/Http/Controllers/AdminModule/AdminController.php
+++ app/Http/Controllers/AdminModule/AdminController.php
@@ -14,7 +14,10 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.home');
+
+        return view('admin.home')->with([
+            'page' => 'dashboard',
+        ]);
     }
 
     /**
