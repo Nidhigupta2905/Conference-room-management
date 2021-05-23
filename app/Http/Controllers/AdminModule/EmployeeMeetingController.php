0b39b373ef29294a6440d5f9bedd06e55bd91104
sanat-biswas <39663269+sanat-biswas@users.noreply.github.com>
2021-05-23 16:15:37 +0530

    refacotring

--- app/Http/Controllers/AdminModule/EmployeeMeetingController.php
+++ app/Http/Controllers/AdminModule/EmployeeMeetingController.php
@@ -234,12 +234,7 @@ class EmployeeMeetingController extends Controller
         $meetings = Meeting::with(['user', 'conferenceRoom'])
             ->orderBy('meeting_date', 'DESC')
             ->paginate(10);
-
-        if ($request->ajax()) {
-            return view('admin.meeting.paginate_data')->with([
-                'meetings' => $meetings,
-            ]);
-        }
+            
         return view('admin.meeting.meeting-history')->with([
             'page' => 'meetingHistory',
             'meetings' => $meetings,
