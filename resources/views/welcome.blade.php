a2a950892a8fd67fe7db9a22e9b4e6b911f50cc6
sanat-biswas <39663269+sanat-biswas@users.noreply.github.com>
2021-05-30 22:20:35 +0530

    fixing home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,15 @@
         }
 
         body {
-            margin: 0
+            margin: 0;
+            background-color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            overflow-y: hidden;
+            
+            /* background-image: url("{{ asset('admin/dist/images/meeting-room.jpg') }}"); */
         }
 
         a {
@@ -37,6 +45,49 @@
             line-height: 1.5
         }
 
+        /* #button {
+            padding: 1em 1.5em;
+            text-decoration: none;
+            background-color: gray;
+            position: relative;
+            box-shadow: 0 5px 0 darkred;
+        } */
+
+
+        .button {
+            border-top: 1px solid #96d1f8;
+            background: #65a9d7;
+            background: -webkit-gradient(linear, left top, left bottom, from(#3e779d), to(#65a9d7));
+            background: -webkit-linear-gradient(top, #3e779d, #65a9d7);
+            background: -moz-linear-gradient(top, #3e779d, #65a9d7);
+            background: -ms-linear-gradient(top, #3e779d, #65a9d7);
+            background: -o-linear-gradient(top, #3e779d, #65a9d7);
+            padding: 20px 40px;
+            -webkit-border-radius: 40px;
+            -moz-border-radius: 40px;
+            border-radius: 40px;
+            -webkit-box-shadow: rgba(0, 0, 0, 1) 0 1px 0;
+            -moz-box-shadow: rgba(0, 0, 0, 1) 0 1px 0;
+            box-shadow: rgba(0, 0, 0, 1) 0 1px 0;
+            text-shadow: rgba(0, 0, 0, .4) 0 1px 0;
+            color: white;
+            font-size: 24px;
+            font-family: Georgia, Serif;
+            text-decoration: none;
+            vertical-align: middle;
+        }
+
+        .button:hover {
+            border-top-color: #28597a;
+            background: #28597a;
+            color: #ccc;
+        }
+
+        .button:active {
+            border-top-color: #1b435e;
+            background: #1b435e;
+        }
+
         *,
         :after,
         :before {
@@ -49,248 +100,7 @@
             text-decoration: inherit
         }
 
-        svg,
-        video {
-            display: block;
-            vertical-align: middle
-        }
-
-        video {
-            max-width: 100%;
-            height: auto
-        }
-
-        .bg-white {
-            --bg-opacity: 1;
-            background-color: #fff;
-            background-color: rgba(255, 255, 255, var(--bg-opacity))
-        }
-
-        .bg-gray-100 {
-            --bg-opacity: 1;
-            background-color: #f7fafc;
-            background-color: rgba(247, 250, 252, var(--bg-opacity))
-        }
-
-        .border-gray-200 {
-            --border-opacity: 1;
-            border-color: #edf2f7;
-            border-color: rgba(237, 242, 247, var(--border-opacity))
-        }
-
-        .border-t {
-            border-top-width: 1px
-        }
-
-        .flex {
-            display: flex
-        }
-
-        .grid {
-            display: grid
-        }
-
-        .hidden {
-            display: none
-        }
-
-        .items-center {
-            align-items: center
-        }
-
-        .justify-center {
-            justify-content: center
-        }
-
-        .font-semibold {
-            font-weight: 600
-        }
-
-        .h-5 {
-            height: 1.25rem
-        }
-
-        .h-8 {
-            height: 2rem
-        }
-
-        .h-16 {
-            height: 4rem
-        }
-
-        .text-sm {
-            font-size: .875rem
-        }
-
-        .text-lg {
-            font-size: 1.125rem
-        }
-
-        .leading-7 {
-            line-height: 1.75rem
-        }
-
-        .mx-auto {
-            margin-left: auto;
-            margin-right: auto
-        }
-
-        .ml-1 {
-            margin-left: .25rem
-        }
-
-        .mt-2 {
-            margin-top: .5rem
-        }
-
-        .mr-2 {
-            margin-right: .5rem
-        }
-
-        .ml-2 {
-            margin-left: .5rem
-        }
-
-        .mt-4 {
-            margin-top: 1rem
-        }
-
-        .ml-4 {
-            margin-left: 1rem
-        }
-
-        .mt-8 {
-            margin-top: 2rem
-        }
-
-        .ml-12 {
-            margin-left: 3rem
-        }
-
-        .-mt-px {
-            margin-top: -1px
-        }
-
-        .max-w-6xl {
-            max-width: 72rem
-        }
-
-        .min-h-screen {
-            min-height: 100vh
-        }
-
-        .overflow-hidden {
-            overflow: hidden
-        }
-
-        .p-6 {
-            padding: 1.5rem
-        }
-
-        .py-4 {
-            padding-top: 1rem;
-            padding-bottom: 1rem
-        }
-
-        .px-6 {
-            padding-left: 1.5rem;
-            padding-right: 1.5rem
-        }
-
-        .pt-8 {
-            padding-top: 2rem
-        }
-
-        .fixed {
-            position: fixed
-        }
-
-        .relative {
-            position: relative
-        }
-
-        .top-0 {
-            top: 0
-        }
-
-        .right-0 {
-            right: 0
-        }
-
-        .shadow {
-            box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)
-        }
-
-        .text-center {
-            text-align: center
-        }
-
-        .text-gray-200 {
-            --text-opacity: 1;
-            color: #edf2f7;
-            color: rgba(237, 242, 247, var(--text-opacity))
-        }
-
-        .text-gray-300 {
-            --text-opacity: 1;
-            color: #e2e8f0;
-            color: rgba(226, 232, 240, var(--text-opacity))
-        }
-
-        .text-gray-400 {
-            --text-opacity: 1;
-            color: #cbd5e0;
-            color: rgba(203, 213, 224, var(--text-opacity))
-        }
-
-        .text-gray-500 {
-            --text-opacity: 1;
-            color: #a0aec0;
-            color: rgba(160, 174, 192, var(--text-opacity))
-        }
-
-        .text-gray-600 {
-            --text-opacity: 1;
-            color: #718096;
-            color: rgba(113, 128, 150, var(--text-opacity))
-        }
-
-        .text-gray-700 {
-            --text-opacity: 1;
-            color: #4a5568;
-            color: rgba(74, 85, 104, var(--text-opacity))
-        }
-
-        .text-gray-900 {
-            --text-opacity: 1;
-            color: #1a202c;
-            color: rgba(26, 32, 44, var(--text-opacity))
-        }
-
-        .underline {
-            text-decoration: underline
-        }
-
-        .antialiased {
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale
-        }
-
-        .w-5 {
-            width: 1.25rem
-        }
-
-        .w-8 {
-            width: 2rem
-        }
-
-        .w-auto {
-            width: auto
-        }
-
-        .grid-cols-1 {
-            grid-template-columns: repeat(1, minmax(0, 1fr))
-        }
+        
 
         @media (min-width:640px) {
             .sm\:rounded-lg {
@@ -354,10 +164,13 @@
         }
 
         @media (min-width:1024px) {
-            .lg\:px-8 {
+            /* .lg\:px-8 {
                 padding-left: 2rem;
                 padding-right: 2rem;
                 margin-bottom: auto;
+            } */
+            #col{
+                    
             }
         }
 
@@ -395,17 +208,12 @@
 
     </style>
 
-    
 
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
 
-    </style>
+
 </head>
 
-<body class="antialiased">
+<body>
 
     @if (session('success'))
         <div class="alert alert-success ">
@@ -429,7 +237,22 @@
             </div>
         @endforeach
     @endif
-    <div
+
+    <div class="container">
+        <div class="row" >
+            <div class="col-md-6" id="col">
+                {{-- <img src="{{asset('admin/dist/images/google.jpg')}}" alt="" srcset=""> --}}
+                <a href="{{ route('auth.google.login') }}" class="btn btn-block button" id="button"
+                    style="font-size: 20px; text-align:left;">
+                    <i class="fab fa-google-plus mr-2"></i> Employee Login
+                </a>
+            </div>
+            <div class="col-md-3">
+                <img src="{{asset('admin/dist/images/google.jpg')}}" alt="" srcset="" height="100px" width="600px">
+            </div>
+        </div>
+    </div>
+    {{-- <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <label for="" class="font-weight-bold" style="font-size: 28px;">CR Management System</label>
@@ -438,6 +261,7 @@
                     <div class="p-6">
                         <div class="flex items-center">
                             <div class="ml-4 text-lg leading-7 font-semibold float-left">
+
                                 <a href="{{ route('auth.google.login') }}" class="btn btn-block " style="font-size: 20px; text-align:left;">
                                     <i class="fab fa-google-plus mr-2"></i> Employee Login
                                 </a>
@@ -446,17 +270,17 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
-        <div class="social-auth-links text-center mb-3">
+    {{-- <div class="social-auth-links text-center mb-3"> --}}
 
 
 
-            {{-- <a href="{{ route('login') }}" class="btn btn-block btn-primary">
+    {{-- <a href="{{ route('login') }}" class="btn btn-block btn-primary">
                 <i class="fab mr-2"></i> Admin Login
 
             </a> --}}
-        </div>
+    {{-- </div> --}}
     </div>
 </body>
 
